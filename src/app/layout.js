292a063a9863f64593e90eca1f3b1300466b11d9
1 <386029724@qq.com>
2025-08-25 13:57:18 +0800

    +google analytics

--- src/app/layout.js
+++ src/app/layout.js
@@ -4,6 +4,7 @@ import 'sanitize.css'
 import './app.scss'
 import './main.scss'
 import {DialogContainer} from "@/app/utils/DialogContainer";
+import Script from "next/script";
 
 export const metadata = {
     title: config.title,
@@ -22,6 +23,19 @@ export default function RootLayout({children}) {
     return (
         <html lang="chs">
         <body>
+        {/* Google Analytics */}
+        <Script
+            src="https://www.googletagmanager.com/gtag/js?id=G-BQJX3Y50XY"
+            strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+            {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-BQJX3Y50XY');
+          `}
+        </Script>
         <StyledComponentsRegistry>
             {children}
         </StyledComponentsRegistry>
