30516835e34a039a8b7d05858716364dad79b4ec
1 <386029724@qq.com>
2025-08-28 13:36:10 +0800

    fix qr cache

--- src/app/components/loginpage/ServerLoginPage.js
+++ src/app/components/loginpage/ServerLoginPage.js
@@ -1,8 +1,13 @@
 import React from 'react';
 import LoginPage from "@/app/components/loginpage/LoginPage";
 import types from "@/app/auth_types/types";
+import {connection} from "next/server";
+
 
 async function ServerLoginPage({type}) {
+
+    await connection()
+    
 
     return (
         <LoginPage type={type} qr={await types[type].getQr()}/>
