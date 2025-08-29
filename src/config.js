8d42e6bcbe1019ce66cc8f4bc2d41fe33a95314c
1 <386029724@qq.com>
2025-08-29 10:21:45 +0800

    Update config.js

--- src/config.js
+++ src/config.js
@@ -3,7 +3,6 @@ import "server-only"
 import axiosRetry from "axios-retry";
 
 export const client = axios.create({
-    withCredentials: true,
     headers: {
         "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/139.0.0.0 Safari/537.36"
     }
